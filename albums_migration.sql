5ab5857186c991a52bd72f2efbd1c063231583dc
Anna Morton <annajmorton@gmail.com>
2016-03-14 11:25:33 -0500

    created album table in codeup-test database

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	artist_name VARCHAR(80) NOT NULL,
+	release_date YEAR(4),
+	sales FLOAT UNSIGNED, 
+	genre VARCHAR(20),
+	PRIMARY KEY (id)
+
+);
