43c4da634b2fc524f67f492bf0aa347b53d2da6f
Anna Morton <annajmorton@gmail.com>
2016-03-15 15:08:08 -0500

    changed file type to sql and added more where/continue excercises

--- where_excercises.sql
+++ where_excercises.sql
@@ -1,73 +1,28 @@
-FROM employees 
-WHERE last_name LIKE '%q%'
-	NOT last_name LIKE '%qu%'
-
-SELECT * 
-FROM employees 
-WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
-	AND birth_date LIKE '%-12-25'
-
-SELECT * 
-FROM employees 
-WHERE last_name LIKE 'E%'
-	AND last_name LIKE '%e'
-
 SELECT * 
 FROM employees 
 WHERE (first_name = 'Irena' 
 	OR first_name = 'Vidya' 
 	OR first_name = 'Maya')
-	AND gender = 'M'
-
-
-SELECT * 
-FROM employees 
-WHERE first_name = 'Irena' OR 'Vidya' OR 'Maya'
-
-SELECT *
-FROM employees 
-WHERE last_name LIKE '%q%'
-
-SELECT * 
-FROM employees 
-WHERE birth_date LIKE '%-12-25'
-
-SELECT * 
-FROM employees 
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-
-SELECT * 
-FROM employees 
-WHERE hire_date BETWEEN 1990-01-01 AND 1999-12-31
-
-SELECT * 
-FROM employees 
-WHERE hire_date &gt; 1989-12-31
+	AND gender = 'M';
 
 SELECT * 
 FROM employees 
 WHERE last_name LIKE 'E%'
+	OR last_name LIKE '%e';	
 
 SELECT * 
 FROM employees 
-WHERE first_name IN ('Irena','Vidya','Maya')
-
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek')
-
-SELECT first_name
-FROM employees
-WHERE first_name LIKE '%sus%'
-
-SELECT emp_no, first_name, last_name
-FROM `employees`
-WHERE emp_no BETWEEN 10026 AND 10082
-
-SELECT DISTINCT first_name
-FROM employees
-WHERE first_name LIKE '%sus%'
-
+WHERE last_name LIKE 'E%'
+	AND last_name LIKE '%e';
+	
+SELECT * 
+FROM employees 
+WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+	AND birth_date LIKE '%-12-25';
+	
 SELECT *
-FROM employees
-WHERE hire_date = '1985-01-01';
+FROM employees 
+WHERE (last_name LIKE '%q%')
+	AND last_name NOT LIKE '%qu%';
+
+	
