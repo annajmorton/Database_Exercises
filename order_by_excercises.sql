1f2464c5789ce34717b86ff5e1c2114b44551100
Anna Morton <annajmorton@gmail.com>
2016-03-15 15:24:14 -0500

    ordered the previous selects

--- order_by_excercises.sql
+++ order_by_excercises.sql
@@ -3,7 +3,22 @@ FROM employees
 WHERE (first_name = 'Irena' 
 	OR first_name = 'Vidya' 
 	OR first_name = 'Maya')
-	AND gender = 'M';
+	AND gender = 'M'
+ORDER BY first_name, last_name;
+
+SELECT * 
+FROM employees 
+WHERE (first_name = 'Irena' 
+	OR first_name = 'Vidya' 
+	OR first_name = 'Maya')
+	AND gender = 'M'
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT * 
+FROM employees 
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;	
+
 
 SELECT * 
 FROM employees 
@@ -18,7 +33,8 @@ WHERE last_name LIKE 'E%'
 SELECT * 
 FROM employees 
 WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
-	AND birth_date LIKE '%-12-25';
+	AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 	
 SELECT *
 FROM employees 
